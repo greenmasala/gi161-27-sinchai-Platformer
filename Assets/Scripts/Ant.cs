0c70acfaf7b129cf37a3c37f8e58a1c5550201d5
greenmasala <sinchai2549@gmail.com>
2025-10-21 15:54:20 +0700

    Ant Movement

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -8,6 +8,9 @@ public class Ant : Enemy
     void Start()
     {
         base.Init(30);
+        DamageHit = 20;
+
+        velocity = new Vector2(-1.0f, 0.0f); //speed of ant. -1 is going to the left
     }
 
     void Update()
@@ -15,8 +18,13 @@ public class Ant : Enemy
 
     }
 
+    void FixedUpdate()
+    {
+        Behavior();
+    }
+
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime); //move from one position to another. (rb.position is position of ant) DeltaTime is basically making it move every frame, though the frames wont be consistent (the .ms rate). FixedDeltaTime is the same thing cept the frames are consistent. Useful for physics related stuff to prevent stuttering or anything that requires smooth movement.
     }
 }
